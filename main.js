532f52fa862e777faa1819de5b9095f5f8d2e3d3
JhonDGomeS <JhonDGomeS@users.noreply.github.com>
2024-04-25 19:57:15 -0300

    ok

--- main.js
+++ main.js
@@ -15,13 +15,14 @@ for (let i = 0; i < botoes.length; i++) {
 
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
-const tempoObjetivo2 = new Date("2024-04-26T23:59:59");
-const tempoObjetivo3 = new Date("2024-04-30T23:59:59");
-const tempoObjetivo4 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo1 = new Date("2024-10-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-05-30T23:59:59");
+const tempoObjetivo3 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo4 = new Date("2026-12-31T23:59:59");
+const tempoObjetivo5 = new Date("2027-12-31T23:59:59");
 
 
-const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo5];
 
 
 function calculaTempo(tempoObjetivo) {
